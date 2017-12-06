9fe36c8c2a7fc036e09769c78c75583724a6eac0
Spencer Smith <WankFerret@Stevens-MacBook.local>
2017-12-06 13:33:56 -0600

    Mini done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-angular.module('myApp', []);
+angular.module('myApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider){
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: './js/home/homeTmpl.html'
+    })
+    .state('products', {
+        url: '/products/:id',
+        templateUrl: './js/products/productsTmpl.html',
+        controller: 'productsCtrl',
+        resolve: {
+            products: function(productsSrvc, $stateParams){
+                return productsSrvc
+                .getProducts($stateParams.id)
+                .then(res=> res.data)
+            }
+        }
+    })
+    .state('settings', {
+        url: '/settings',
+        templateUrl: './js/settings/settingsTmpl.html'
+    })
+    $urlRouterProvider.otherwise('/')
+});
---
+++ js/products/productsCtrl.js
@@ -0,0 +1,13 @@
+// angular.module('myApp')
+// .controller('productsCtrl', function($scope, productsSrvc, $stateParams){
+//     $stateParams.id
+//     productsSrvc.getProducts($stateParams.id).then(res => {
+//         $scope.products = res.data;
+//     })
+// })
+angular
+  .module("myApp")
+  .controller("productsCtrl", function($scope, products) {
+      
+    
+  });
